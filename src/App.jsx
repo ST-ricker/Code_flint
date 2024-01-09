8bc718cd27f728f489f912d52aca238044269802
Yugal Joshi <80611659+ST-ricker@users.noreply.github.com>
2024-01-09 16:02:33 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WalletBalance from './components/WalletBalance';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Your App</h1>
+      <WalletBalance />
+    </div>
+  );
+};
+
+export default App;
